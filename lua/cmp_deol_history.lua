69d7b3d8823292b35270fda9108cda8a9d89351a
tamago324 <tamago_pad@yahoo.co.jp>
2021-11-03 08:34:05 +0900

    perf: remove duplicate candidates

--- lua/cmp_deol_history.lua
+++ lua/cmp_deol_history.lua
@@ -11,9 +11,25 @@ M.get_keyword_pattern = function()
   return [[.*]]
 end
 
+
+local unique_list = function(list)
+  local res = {}
+  -- table にしておく
+  local added_histories = {}
+  for _, v in ipairs(list) do
+    if added_histories[v] == nil then
+      -- 追加されていなければ、追加する
+      table.insert(res, v)
+      added_histories[v] = true
+    end
+  end
+
+  return res
+end
+
 M.complete = function(self, request, callback)
   local q = request.context.cursor_before_line
-  local histories = vim.fn["deol#_get_histories"]()
+  local histories = unique_list(vim.fn["deol#_get_histories"]())
   local words = {}
   for _, val in ipairs(histories) do
     if vim.startswith(val, q) then
@@ -25,5 +41,10 @@ M.complete = function(self, request, callback)
 
   callback(words)
 end
+
+-- XXX: 必要かどうかわからない...
+-- M.resolve = function(self, completion_item, callback)
+--   callback(completion_item)
+-- end
 
 return M
